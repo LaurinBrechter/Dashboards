5634febed01a079abd9a2b772c7f651f231c5281
Laurin <brechterlaurin@gmail.com>
2022-05-24 12:48:28 +0200

    added sparklines and pie chart to dashboard, updated sc creation function

--- SC_Resilience_Dashboard.py
+++ SC_Resilience_Dashboard.py
@@ -6,15 +6,16 @@ import pandas as pd
 import plotly.express as px
 import networkx as nx
 import numpy as np
-from sc_time_series_creator import SupplyChain, trend, trend, seasonal_pattern, seasonality, noise
+from sc_time_series_creator import SupplyChain, trend, seasonal_pattern, seasonality, noise
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
-
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
-g = SupplyChain(5,3).graph
+g = SupplyChain(n_tiers=5, avg_n_sups=4, avg_decay=0.5, lbound_add_edges=1, ubound_add_edges=2).graph
+
 positions = nx.spring_layout(g)
+
 node_data = [{"data": {"id": str(i), "label": str(i)}, 
               "position": {"x": positions[i][0]*1000, "y": positions[i][1]*1000}, 
               "locked":True,
@@ -24,7 +25,6 @@ node_data = [{"data": {"id": str(i), "label": str(i)},
                         'height': 30,
                         "color": "white",
                         }
-
               } 
               for i in list(g.nodes())]
 
@@ -39,11 +39,8 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 
-
 SIDEBAR_STYLE = {
-    
     "textAlign":"left"
-    # "background-color": "#f8f9fa"
 }
 
 HEADER_STYLE = {
@@ -51,20 +48,44 @@ HEADER_STYLE = {
     "textAlign": "center"
 }
 
-GRAPH_STYLE = go.Layout(title="First Graph", 
-              margin={"b": 0, "l":10, "r": 10, "t":20},
-              font={"color": "#adafae"},
-              paper_bgcolor="#0B243B",
-              plot_bgcolor="#0B243B"
+GRAPH_STYLE = go.Layout( 
+    margin={"b": 0, "l":10, "r": 10, "t":20},
+    font={"color": "#adafae"},
+    paper_bgcolor="#0B243B",
+    plot_bgcolor="#0B243B",
+    height=400
     )
     
-
-fig1 = go.Figure(data=go.Scatter(y=[1,2,3,4]), 
-                layout=GRAPH_STYLE
+SPARKLINE_STYLE = go.Layout(
+    paper_bgcolor="#060606", # #060606
+    plot_bgcolor="#060606", # #060606
+    margin={"b": 0, "l":0, "r": 0, "t":0},
+    height=100,
+    yaxis={"visible":False},
+    xaxis={"visible":False}
 )
 
-fig2 = go.Figure(data=go.Scatter(y=[1,2,3,4]), 
-                layout=GRAPH_STYLE
+# color_discrete_map={'Thur':'lightcyan',
+#                                  'Fri':'cyan',
+#                                  'Sat':'royalblue',
+#                                  'Sun':'darkblue'})
+
+fig1 = go.Figure(
+    data=go.Scatter(y=[1,2,3,4]), 
+    layout=GRAPH_STYLE
+)
+
+grah_data = pd.DataFrame([[i[0], i[1]["tier"]] for i in list(g.nodes(data=True))])[1].value_counts(normalize=True)
+
+# create a random spark line with a random walk ending at the average clustering of the network.
+avg_clust = nx.average_clustering(g)
+spark_data = [avg_clust]
+for i in range(10):
+    spark_data.append(spark_data[-1] + np.random.normal(loc=0, scale=avg_clust/8))
+
+spark_intercon = go.Figure(
+    data=go.Scatter(y=spark_data[::-1]),
+    layout=SPARKLINE_STYLE
 )
 
 
@@ -77,7 +98,7 @@ app.layout = html.Div(
             dbc.Col([
                 html.H1("Network", style=HEADER_STYLE),
                 dcc.Graph(figure=fig1),
-                dcc.Graph(figure=fig2)
+                dcc.Graph(id="tier-distribution-pie")
 
             ], width=3),
 
@@ -120,7 +141,11 @@ app.layout = html.Div(
                         [
                             dbc.AccordionItem(
                                 [
-                                    html.P("This is the content of the first section")
+                                    html.Div([
+                                        html.P("Network Clustering Coefficient: {}".format(np.round(nx.average_clustering(g), 3))), 
+                                        dcc.Graph(figure=spark_intercon)
+                                    ])
+                                    
                                 ], 
                                 title="Interconnectivity"
                             ),
@@ -156,7 +181,6 @@ app.layout = html.Div(
                     )
                     ], style=SIDEBAR_STYLE
                 )], width=3)
-
         ])
     ]
 )
@@ -164,18 +188,37 @@ app.layout = html.Div(
 
 @app.callback(
     Output('node-info','children'),
+    Output('tier-distribution-pie','figure'),
     Input('org-chart','tapNodeData'),
 )
 def update_nodes(data):
     print(data)
+
+    pull = [0] * len(grah_data)
+    
     if data:
         node = int(data["id"])
-        return ["Node Degree: {}".format(nx.degree(g)[node]), 
-                html.P("This is nice"),
-                ]
-    else:
-        return [html.Div("Select a Node to see Degree")]
+        tier = g.nodes(data=True)[node]["tier"]
+        pull[int(tier[-1])] = 0.3
 
+        # pull out a piece of the pie of the node belonging to that tier is selected.
+        
+        return_data = ["Node Degree: {}".format(nx.degree(g)[node]), 
+                html.P("Node Clustering {}".format(nx.clustering(g, node))),
+                html.P("Node Tier: {}".format(g.nodes(data=True)[node]["tier"]))
+        ]
+    else:
+        return_data = [html.Div("Select a Node to see Degree")]
+
+    pie_fig = go.Figure(data=[go.Pie(
+                        values=grah_data.values[::-1], 
+                        labels=grah_data.index[::-1], 
+                        hole=.4,
+                        pull = pull
+                    )], 
+                layout=GRAPH_STYLE)
+
+    return return_data, pie_fig
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- sc_time_series_creator.py
+++ sc_time_series_creator.py
@@ -1,8 +1,9 @@
 import networkx as nx
 import numpy as np
+from random import randint
 
 class SupplyChain():
-    def __init__(self, n_tiers, avg_n_sups):
+    def __init__(self, n_tiers, avg_n_sups, avg_decay=0, ubound_add_edges = 0, lbound_add_edges = 0):
         self.graph = nx.Graph()
         self.graph.add_node(0, tier="TIER0")
         self.n_tiers = n_tiers
@@ -10,9 +11,12 @@ class SupplyChain():
         self.tier_names = ["TIER"+str(i) for i in range(self.n_tiers)]
         
         # create the supply side
+        a = lbound_add_edges
+        b = ubound_add_edges
+
         comp_id = 1
         for i, tier_name in enumerate(self.tier_names[:-1]):
-            
+            avg_n_sups -= avg_decay
             # search for companies in that tier
             nodes_in_tier = [node for node in self.graph.nodes(data=True) if node[1]["tier"] == tier_name]
             
@@ -20,13 +24,37 @@ class SupplyChain():
             for node in nodes_in_tier:
                 n_sups = np.random.poisson(avg_n_sups)
                 
-                self.graph.add_nodes_from(range(comp_id, n_sups+comp_id), tier="TIER" + str(i+1))
-                    
-                to_add = [(node[0], j, {"inf_sharing":np.random.random()}) for j in range(comp_id, n_sups+comp_id)]
 
+                self.graph.add_nodes_from(range(comp_id, n_sups+comp_id), tier="TIER" + str(i+1))
+                
+                to_add = [(node[0], j, {"inf_sharing":np.random.random()}) for j in range(comp_id, n_sups+comp_id)]
+        
                 self.graph.add_edges_from(to_add)
                 
                 comp_id += n_sups
+            
+            # add additional cross edges.
+            if b > 0:
+                for node in nodes_in_tier:
+                    # extra_conns = np.random.randint(a,b)
+                    extra_conns = randint(a,b)
+                    extra_nodes = np.random.choice(list(set(list(self.graph.nodes())) - set([node[0]])), extra_conns)
+
+                    self.graph.add_edges_from([(node[0], j) for j in extra_nodes])
+                    comp_id += extra_conns
+
+        # also add edges for the last layer
+        if b > 0:
+            last_tier_nodes = [node for node in self.graph.nodes(data=True) if node[1]["tier"] == tier_name]
+            # last_tier_nodes = list(filter(lambda x: x[1]["tier"] == sc.tier_names[-1], list(sc.graph.nodes(data=True))))
+            for node in last_tier_nodes:
+                # extra_conns = np.random.randint(a,b)
+                extra_conns = randint(a,b)
+                extra_nodes = np.random.choice(list(set(list(self.graph.nodes())) - set([node[0]])), extra_conns)
+                self.graph.add_edges_from([(node[0], j) for j in extra_nodes])
+                comp_id += extra_conns
+            
+        # self.graph.remove_edges_from(nx.selfloop_edges(self.graph))
 
 def trend(time, slope=0):
     return slope * time
