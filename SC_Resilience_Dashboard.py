bf538cc18cb0b911cafd74a3fafe5a1c4ca293fd
Laurin <brechterlaurin@gmail.com>
2022-06-16 09:18:14 +0200

    updated styling and css

--- SC_Resilience_Dashboard.py
+++ SC_Resilience_Dashboard.py
@@ -15,7 +15,6 @@ g = SupplyChain(n_tiers=5, avg_n_sups=2, avg_decay=0.3, lbound_add_edges=0, ubou
 
 positions = nx.spring_layout(g)
 
-print(g.nodes(data=True))
 
 node_data = [{"data": {"id": str(i), "label": str(i)}, 
               "position": {"x": positions[i][0]*1000, "y": positions[i][1]*1000}, 
@@ -43,20 +42,21 @@ CONTENT_STYLE = {
 }
 
 SIDEBAR_STYLE = {
-    "textAlign":"left"
+    "textAlign":"left",
+    "color": "#fff"
 }
 
 HEADER_STYLE = {
-    "color": "#adafae",
+    "color": "#fff",
     "textAlign": "center"
 }
 
 GRAPH_STYLE = go.Layout( 
-    margin={"b": 10, "l":10, "r": 10, "t":10},
-    font={"color": "#adafae"},
-    paper_bgcolor="#0B243B",
-    plot_bgcolor="#0B243B",
-    height=400
+    margin={"b": 10, "l":10, "r": 10, "t":50},
+    font={"color": "#fff"},
+    paper_bgcolor="#121212",
+    plot_bgcolor="#121212",
+    height=400,
     )
     
 SPARKLINE_STYLE = go.Layout(
@@ -73,7 +73,8 @@ GRAPH_STYLE_SHEET = [
     {
         'selector': 'node',
         'style': {
-            'content': 'data(label)'
+            'content': 'data(label)',
+            
         }
     },
 
@@ -82,15 +83,20 @@ GRAPH_STYLE_SHEET = [
         'selector': '.red',
         'style': {
             'background-color': '#990000',
-            'line-color': '#990000'
+            'line-color': '#990000',
+            
         }
     },
-    # {
-    #     'selector': '.triangle',
-    #     'style': {
-    #         'shape': 'triangle'
-    #     }
-    # },
+
+    {
+        "selector": ".normal",
+        "style": {
+            "background-color": "#bcd890",
+            "line-color": "#bcd890",
+            "opacity": 0.7
+        }
+    },
+
     {
         "selector": "edge",
         "style": {
@@ -103,15 +109,6 @@ GRAPH_STYLE_SHEET = [
     }
 ]
 
-# color_discrete_map={'Thur':'lightcyan',
-#                                  'Fri':'cyan',
-#                                  'Sat':'royalblue',
-#                                  'Sun':'darkblue'})
-
-fig1 = go.Figure(
-    data=go.Scatter(y=[1,2,3,4]), 
-    layout=GRAPH_STYLE
-)
 
 grah_data = pd.DataFrame([[i[0], i[1]["tier"]] for i in list(g.nodes(data=True))])[1].value_counts(normalize=True)
 
@@ -127,31 +124,41 @@ spark_intercon = go.Figure(
 )
 
 
-app.layout = html.Div(
-    [
-        html.Div(html.Br()),
+app.layout = html.Div([
+    
+        dbc.Row([
+            html.Div(html.Br()),
+            dbc.Col(html.H1("Network", style=HEADER_STYLE), width=3),
+            dbc.Col(html.H1("Supply Chain Monitoring", style=HEADER_STYLE), width=6),
+            dbc.Col(html.H1("Node", style=HEADER_STYLE), width=3)
+        ], 
+        style={"background-color":"#606060"},
+        class_name="row-top",
+        id="nice"),
 
         dbc.Row([
 
             dbc.Col([
-                html.H1("Network", style=HEADER_STYLE),
-                dcc.Graph(figure=fig1),
-                dcc.Graph(id="tier-distribution-pie")
+                # dcc.Graph(figure=fig1),
+                dcc.Graph(id="tier-distribution-pie", style={"box-shadow": "#606060 0px 0px 20px 0px", 
+                                                             "border-radius": "10px", 
+                                                             "border-width": "10px", 
+                                                             "border-color": "rgb(41, 56, 55)"
+                                                             }),
+                html.P("Hello")
 
-            ], width=3),
+            ], width=3, style={"padding-top": "12px"}), # "margin-left":"12px"
 
             dbc.Col([
                 
                 dbc.Row([
                     html.Div(),
-                    html.H1("Supply Chain Monitoring", style=HEADER_STYLE),
-                    
                 ]),
                 
                 cyto.Cytoscape(
                     id='org-chart',
                     layout={'name': 'preset'},
-                    style={'width': '100%', 'height': '700px', "background-color":"#0B243B"},
+                    style={'width': '100%', 'height': '700px', "background-color":"#121212"},
                     stylesheet=GRAPH_STYLE_SHEET,
                     elements=[
         
@@ -159,15 +166,10 @@ app.layout = html.Div(
                 
                     ]
                     
-                )], width=6, style={"padding": "0rem 0rem"}),
+                )], width=6, style={"padding-top": "12px"}),
             dbc.Col([
                 html.Div([
 
-                    html.H1("Node", style=HEADER_STYLE),
-                    
-                    html.Hr(),
-                    html.Br(),
-                    
                     dbc.Accordion(
                         [
                             dbc.AccordionItem(
@@ -219,33 +221,31 @@ app.layout = html.Div(
                             )
                         ]
                     )
-                    ], style=SIDEBAR_STYLE
-                )], width=3, style={"margin-right": "0", "padding": "0rem 0rem"}),
-
-                
-        ]),
-
-        dbc.Row([
-            dcc.Dropdown(
-                id='dropdown-update-layout',
-                value='spring',
-                clearable=False,
-                options=[
-                    {'label': name, 'value': name} for name in ['planar', 'random', 'spectral', 'spring', 'shell']
-                ])
-     ]) 
+                    ], style={"right": "20px", "box-shadow": "#606060 0px 0px 20px 0px"}
+                )], width=3, style={"padding-top": "12px", "right":"20px"}),      
+        ])
+     #    dbc.Row([
+     #        dcc.Dropdown(
+     #            id='dropdown-update-layout',
+     #            value='spring',
+     #            clearable=False,
+     #            options=[
+     #                {'label': name, 'value': name} for name in ['planar', 'random', 'spectral', 'spring', 'shell']
+     #            ])
+     # ]) 
     ]
 )
 
-print(g.nodes())
-
+dd_old = "spring"
 
 @app.callback(
     Output('node-info','children'),
     Output('tier-distribution-pie','figure'),
     Output("supplier-incoming", "figure"),
     Output("supplier-outgoing", "figure"),
+    Output('org-chart', 'elements'),
     Input('org-chart','tapNodeData'),
+    # Input('dropdown-update-layout', 'value')
 )
 def update_data(data):
     """
@@ -254,12 +254,20 @@ def update_data(data):
     https://dash.plotly.com/cytoscape/reference
     """
 
+    # 
+    # if dd_value != dd_old:
+    #     positions = getattr(nx, dd_value + "_layout")(g)
+    # 
+
+    # dd_old = dd_value
+
     pull = [0] * len(grah_data)
     
     if data:
         node = int(data["id"])
         tier = g.nodes(data=True)[node]["tier"]
         pull[int(tier[-1])] = 0.3
+
 
         # pull out a piece of the pie of the node belonging to that tier is selected.
         return_data = ["Node Degree: {}".format(nx.degree(g)[node]), 
@@ -270,6 +278,7 @@ def update_data(data):
     else:
         return_data = [html.Div("Select a Node to see Degree")]
         node = 0
+        tier = "TIER0"
 
     pie_fig = go.Figure(data=[go.Pie(
                         values=grah_data.values[::-1], 
@@ -279,6 +288,14 @@ def update_data(data):
                     )], 
                 layout=GRAPH_STYLE)
 
+    pie_fig.update_layout({
+        "title": "Tier Distribution",
+        "title_x":0.5,
+        "paper_bgcolor":"#606060"
+        # plot_bgcolor="#121212"
+        #606060
+    })
+
     inc = edge_df.loc[edge_df["from"] == node]
     inc["prod_norm"] = inc["production"]/sum(inc["production"])
 
@@ -286,61 +303,118 @@ def update_data(data):
     out["prod_norm"] = out["production"]/sum(out["production"])
     # dist_fig = px.pie(names="from", values="production", data_frame=inc)
 
-    if True:
-        in_fig = go.Figure(data=[go.Bar(
-                            y=inc["to"],
+    if len(inc) > 0:
+        out_fig = go.Figure(data=[go.Bar(
+                            y=[str(i) for i in inc["to"]],
                             x=inc["prod_norm"],
-                            orientation="h"
+                            orientation="h",
+                            name="actual"
                         )], 
                     layout=GRAPH_STYLE)
 
-        in_fig.update_layout({"height": 200})
+        out_fig.add_trace(go.Scatter(
+                            y=[str(i) for i in inc["to"]],
+                            # mode="markers",
+                            x=[1/len(inc["to"]) for i in range(len(inc["to"]))],
+                            name="optimal"
+                        ))
 
-        print(out["prod_norm"])
+
+    else:
+        out_fig = go.Figure(layout=GRAPH_STYLE)
+
+    out_fig.update_layout({
+            "height": 200,
+            "title": "Dist. of outgoing Links",
+            "title_x":0.5,
+            "xaxis_tickformat":'p',
+            "xaxis_title":"% of Sales"
+        })
 
     if len(out) > 0:
-        out_fig = go.Figure(data=[go.Bar(
+        in_fig = go.Figure(data=[go.Bar(
                             x=out["prod_norm"],
                             y=[str(i) for i in out["from"]],
-                            orientation="h"
+                            orientation="h",
+                            name="actual"
                             # hole=.4
                         )], 
                     layout=GRAPH_STYLE)
 
-        out_fig.update_layout({"height": 200})
+
+        in_fig.add_trace(go.Scatter(
+                            y=[str(i) for i in out["from"]],
+                            # mode="markers",
+                            x=[1/len(out["from"]) for i in range(len(out["from"]))],
+                            name="optimal"
+                        ))
+
+        
     else:
-        out_fig = {}
+        in_fig = go.Figure(layout=GRAPH_STYLE)
+        
+    in_fig.update_layout({
+            "height": 200,
+            "title": "Dist. of incoming Links",
+            "xaxis_tickformat":'p',
+            "title_x":0.5,
+            "xaxis_title":"% of Supply",
+        })
 
-    return return_data, pie_fig, in_fig, out_fig 
+    # return return_data, pie_fig, in_fig, out_fig
 
-
-@app.callback(Output('org-chart', 'elements'),
-              Input('dropdown-update-layout', 'value'))
-def update_layout(layout):
-
-    print(layout)
-
-    positions = getattr(nx, layout + "_layout")(g)
-
-    node_data = [{"data": {"id": str(i), "label": str(i)}, 
-          "position": {"x": positions[i][0]*1000, "y": positions[i][1]*1000}, 
+    node_data = []
+    for i in g.nodes(data=True):
+        
+        if i[1]["tier"] == tier:
+            classes = "red"
+        else:
+            classes = "normal"
+        
+        node_data.append({
+          "data": {"id": str(i[0]), 
+                   "label": str(i[0])
+                   }, 
+          "position": {"x": positions[i[0]][0]*1000, 
+                       "y": positions[i[0]][1]*1000
+                       }, 
           "locked":False,
-          'classes': 'red',
+          'classes': classes,
           "style": {"shape": "circle",
                     'width': 30,
                     'height': 30,
-                    "color": "white",
+                    "color": "white", # font color
+                    "opacity": 0.8
                     }
-          } 
-          for i in list(g.nodes())]
+          })
+
+    return return_data, pie_fig, out_fig, in_fig, [*node_data,*edge_data] 
 
 
-    return [
-
-        *node_data,
-        *edge_data
-        
-    ]
+# @app.callback(Output('org-chart', 'elements'),
+#               Input('dropdown-update-layout', 'value'))
+# def update_layout(layout):
+# 
+#     positions = getattr(nx, layout + "_layout")(g)
+# 
+#     node_data = [{"data": {"id": str(i), "label": str(i)}, 
+#           "position": {"x": positions[i][0]*1000, "y": positions[i][1]*1000}, 
+#           "locked":False,
+#           'classes': 'red',
+#           "style": {"shape": "circle",
+#                     'width': 30,
+#                     'height': 30,
+#                     "color": "white", # font color
+#                     }
+#           } 
+#           for i in list(g.nodes())]
+# 
+#     return [
+# 
+#         *node_data,
+#         *edge_data
+#         
+#     ]
 
 
 if __name__ == "__main__":
